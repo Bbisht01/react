5a1cc91238bc637724417e6e3aed5d2ae643fdfe
Babita Bisht <84259086+Bbisht01@users.noreply.github.com>
2022-06-20 10:32:00 +0530

    from

--- src/Day4/ReactForm.jsx
+++ src/Day4/ReactForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './InputForm.css';
 
 export class ReactForm extends Component {
   constructor() {
@@ -7,53 +8,105 @@ export class ReactForm extends Component {
       user: {
         name: '',
         age: '',
+        gender: '',
+        height: '',
         city: '',
       },
     };
   }
   changeName = (e) => {
     console.log(e);
-    this.setState({ ...this.state.user, name: e.target.value });
-    console.log('name');
+    this.setState((prevState) => ({
+      user: { ...prevState.user, name: e.target.value },
+    }));
+    // console.log('name');
   };
   changeCity = (e) => {
-    this.setState({ ...this.state.user, city: e.target.value });
-    console.log(this.state.city);
+    this.setState((prevState) => ({
+      user: { ...prevState.user, city: e.target.value },
+    }));
+    // console.log(this.state.city);
   };
   changeAge = (e) => {
-    this.setState({ ...this.state.user, age: e.target.value });
-    console.log('age');
+    this.setState((prevState) => ({
+      user: { ...prevState.user, age: e.target.value },
+    }));
+    // console.log('age');
   };
-  onSubmit = () => {
-    alert('submitted!!! ', this.state.user.toString());
+  changeGender = (e) => {
+    this.setState((prevState) => ({
+      user: { ...prevState.user, gender: e.target.value },
+    }));
+    // console.log(this.state.gender);
+  };
+  changeHeight = (e) => {
+    this.setState((prevState) => ({
+      user: { ...prevState.user, height: e.target.value },
+    }));
+    // console.log(this.state.height);
+  };
+  onSubmit = (e) => {
+    // this.setState({ ...this.state.user, user: e.target.value });
+    const output = this.state.user;
+    console.log(output);
+    // alert('submitted!!! ', this.state.user.toString());
   };
   clear = () => {
     this.setState({
       user: {
         name: '',
         age: '',
+        gender: '',
+        height: '',
         city: '',
       },
     });
   };
   render() {
     return (
-      <div>
+      <div className="formCard">
         Name:{' '}
         <Input
+          type="text"
           funcName={this.changeName}
           name={'nameBox'}
           val={this.state.name}
         />
+        <br />
         Age:{' '}
-        <Input funcName={this.changeAge} name={'ageBox'} val={this.state.age} />
+        <Input
+          type="text"
+          funcName={this.changeAge}
+          name={'ageBox'}
+          val={this.state.age}
+        />
+        <br />
+        Gender:{' '}
+        <Input
+          type="text"
+          funcName={this.changeGender}
+          name={'genderBox'}
+          val={this.state.gender}
+        />
+        <br />
+        Height:{' '}
+        <Input
+          type="text"
+          funcName={this.changeHeight}
+          name={'heightBox'}
+          val={this.state.height}
+        />
+        <br />
         City :
         <Input
+          type="text"
           funcName={this.changeCity}
           name={'cityBox'}
           val={this.state.city}
         />
+        <br />
         <Button label={'submit'} formAction={this.onSubmit} />
+        <br />
         <Button label={'clear'} formAction={this.clear} />
       </div>
     );
@@ -61,12 +114,16 @@ export class ReactForm extends Component {
 }
 
 function Input(props) {
-  console.log(...[props]);
+  // console.log(...[props]);
   return (
     <>
       <input
         onChange={(event) => props.funcName(event)}
         name={props.name}
+        age={props.age}
+        gender={props.gender}
+        height={props.height}
+        city={props.city}
         value={props.val}
       />
     </>
